170c4f4cd49f31f3ef3765525bb37515676cc2c9
letsgitthis <jbentley1011@gmail.com>
2021-02-12 11:44:17 -0600

    added nodeIntegration true to run scripts in html

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-const {app, BrowserWindow} = require('electron');
+// By requiring Electron, we are telling the script that we
+// want to place our program in a window rather than a browser.
+const {app, BrowserWindow } = require('electron');
 const url = require('url');
 const path = require('path');
 
 let win
 
+// We then write a function specifying the size of the window
+// as well as the path our app is - to be placed in the window.
+// nodeIntegration must be enabled for scripts in HTML to run.
 function createWindow() {
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({
+        width: 800, 
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
     win.loadURL(url.format ({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
